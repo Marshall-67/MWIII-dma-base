f1dd316d007ee5a532da5b8ca5a1efe3965ae1d2
Brendanj9554 <112026999+Brendanj9554@users.noreply.github.com>
2024-02-19 12:42:35 +0000

    Add files via upload

--- game.hpp
+++ game.hpp
@@ -129,74 +129,72 @@ namespace game {
 				if (!bone_pointer)
 					continue;
 
-				if (Utilities->w2s(position, player_screen)) // Convert base position to screen coordinates
-				{
-					NameEntry player_name_entry = entity.get_name_entry(i);
-
-					if (player_name_entry.name == NULL)
-						continue;
-
-					auto entity_distance = Utilities->units_to_m(Local_Player_Position.distance_to(position));
-
-					if (entity_distance < globals->maximum_distance)
+					if (Utilities->w2s(position, player_screen))
 					{
+						NameEntry player_name_entry = entity.get_name_entry(i);
 
-						if (globals->box) {
-							fvector2d head_screen;
-							auto head_position = Utilities->retrieve_bone_position(bone_pointer, Entity_Bone_Position, 7); 
-							if (Utilities->w2s(head_position, head_screen)) {
-								auto player_height = abs(head_screen.y - player_screen.y);
-								auto player_width = player_height * 0.65;
-								auto box_top_left_x = player_screen.x - (player_width / 2);
-								auto box_top_left_y = player_screen.y - player_height; 
-								draw->draw_box(box_top_left_x, box_top_left_y, player_width, player_height, ImColor(255, 255, 255, 255), 1.0f);
-								auto box_bottom_edge_y = box_top_left_y + player_height;
+						if (player_name_entry.name == NULL)
+							continue;
 
-								if (globals->username) {
-									ImGui::GetBackgroundDrawList()->AddText(
-										ImVec2(player_screen.x - (ImGui::CalcTextSize(player_name_entry.name).x / 2), box_top_left_y - 20), // 20 pixels above the box for padding
-										ImColor(255, 255, 0, 255),
-										player_name_entry.name);
-								}
-								if (globals->distance) {
-									ImGui::GetBackgroundDrawList()->AddText(
-										ImVec2(player_screen.x - (ImGui::CalcTextSize(Utilities->ConvertDistanceToString((int)entity_distance).c_str()).x / 2), box_bottom_edge_y + 5), // 5 pixels below the box for padding
-										ImColor(255, 255, 255, 255),
-										Utilities->ConvertDistanceToString((int)entity_distance).c_str());
-								}
+						auto entity_distance = Utilities->units_to_m(Local_Player_Position.distance_to(position));
+
+						if (entity_distance < globals->maximum_distance)
+						{
+							if (globals->distance) {
+								ImGui::GetBackgroundDrawList()->AddText(ImVec2(player_screen.x - (ImGui::CalcTextSize(Utilities->ConvertDistanceToString((int)entity_distance).c_str()).x / 2 + 1), player_screen.y), ImColor(255, 255, 255, 255), Utilities->ConvertDistanceToString((int)entity_distance).c_str());
 							}
-						}
+
+							if (globals->username) {
+								ImGui::GetBackgroundDrawList()->AddText(
+									ImVec2(player_screen.x - (ImGui::CalcTextSize(player_name_entry.name).x / 2), player_screen.y - 20), // Adjust position as necessary
+									ImColor(255, 255, 0, 255), // Color changed to yellow
+									player_name_entry.name
+								);
+							}
+							if (globals->box) {
+								fvector2d head_screen;
+								auto head_position = Utilities->retrieve_bone_position(bone_pointer, Entity_Bone_Position, 7);
+								if (Utilities->w2s(head_position, head_screen)) {
+									auto player_height = abs(head_screen.y - player_screen.y);
+									auto player_width = player_height * 0.65;
+									auto box_top_left_x = player_screen.x - (player_width / 2);
+									auto box_top_left_y = player_screen.y - player_height;
+									draw->draw_box(box_top_left_x, box_top_left_y, player_width, player_height, ImColor(255, 255, 255, 255), 1.0f);
+									auto box_bottom_edge_y = box_top_left_y + player_height;
+							}
 
 
-						ImColor ESPColor = ImColor(255, 0, 0, 255);
-						if (globals->line_to_center) {
-							ImGui::GetBackgroundDrawList()->AddLine(ImVec2(screen_middle.x, screen_middle.y), ImVec2(player_screen.x, player_screen.y), ESPColor, 2.0f);
-						}
-						if (globals->line_to_bottom) {
-							ImGui::GetBackgroundDrawList()->AddLine(ImVec2(screen_middle.x, decrypt_refdef->ref_def_nn.height), ImVec2(player_screen.x, player_screen.y), ESPColor, 2.0f);
-						}
+							ImColor ESPColor = ImColor(255, 0, 0, 255); 
+							if (globals->line_to_center) {
+								ImGui::GetBackgroundDrawList()->AddLine(ImVec2(screen_middle.x, screen_middle.y), ImVec2(player_screen.x, player_screen.y), ESPColor, 2.0f);
+							}
+							if (globals->line_to_bottom) {
+								ImGui::GetBackgroundDrawList()->AddLine(ImVec2(screen_middle.x, decrypt_refdef->ref_def_nn.height), ImVec2(player_screen.x, player_screen.y), ESPColor, 2.0f);
+							}
 
-						if (globals->aimbot) {
+							if (globals->aimbot) {
 
-							fvector2d select_bone;
-							if (entity.team_id() != Local_Player_Team)
-								continue;
-							if (Utilities->w2s(Utilities->retrieve_bone_position(bone_pointer, Entity_Bone_Position, 7), select_bone)) {
-								if (globals->aimbot_key) {
+								fvector2d select_bone;
+								if (entity.team_id() != Local_Player_Team)
+									continue;
+								if (Utilities->w2s(Utilities->retrieve_bone_position(bone_pointer, Entity_Bone_Position, 7), select_bone)) {
+									if (globals->aimbot_key) {
 
-									if (select_bone.x > 1 && select_bone.y > 1)
-									{
-										Utilities->cursor_to(select_bone.x, select_bone.y);
+										if (select_bone.x > 1 && select_bone.y > 1)
+										{
+											Utilities->cursor_to(select_bone.x, select_bone.y);
+										}
 									}
 								}
 							}
 						}
 					}
-				}
-				ImGui::GetForegroundDrawList()->AddText(ImVec2(100, 100), IM_COL32(255, 255, 0, 255), "ActorLoop completed");
+					ImGui::GetForegroundDrawList()->AddText(ImVec2(100, 100), IM_COL32(255, 255, 0, 255), "ActorLoop completed");
 
+				}
 			}
+
 		}
-	
+
 	};
 } static game::c_game* Game = new game::c_game();
